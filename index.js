a59aec437a0862dcd941735f0d667e668e702c69
regex <86455212+REGEX777@users.noreply.github.com>
2021-08-06 15:56:29 +0530

    added all the files

---
+++ index.js
@@ -0,0 +1,10 @@
+// const got = require("got");
+// const fetch = require('fetch')
+
+const button = document.getElementById('#btn')
+const audio = new Audio("audio/sound.mp3")
+
+async function insertData(){
+    audio.play()
+    document.getElementById('p').innerHTML = "i just wanna playe games on ur ipad if its ok?";
+}
