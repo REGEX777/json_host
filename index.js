211a7a890492cc06af770b3a3696a36df987b1da
regex <86455212+REGEX777@users.noreply.github.com>
2021-08-06 19:21:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const audio = new Audio("audio/sound.mp3")
 
 function insertData(){
     audio.play()
-    document.getElementById('p').innerHTML = "i just wanna play games on ur ipad if its ok?";
+    document.getElementById('p').innerHTML = "अमेरिका में क्या आपके टूथपेस्ट में नमक था?";
 }
