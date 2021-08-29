508dc3910cf24a5febf478ce52cbb5dee25ad9bd
regex <86455212+REGEX777@users.noreply.github.com>
2021-08-29 09:53:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const audio = new Audio("audio/sound.mp3")
 
 function insertData(){
     audio.play()
-    document.getElementById('p').innerHTML = "अमेरिका में क्या आपके टूथपेस्ट में नमक था?";
+    document.getElementById('p').innerHTML = "क्या आपके टूथपेस्ट में नमक था?";
 }
